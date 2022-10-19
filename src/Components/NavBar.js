189e0b1fb6d6710002d87a12a5097901f44ea34c
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-10-19 13:34:55 +1300

    config favicon, mobile responsive margins & nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,24 +1,27 @@
 import { Link } from 'gatsby'
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import * as styles from '../styles/navbar.module.css'
 
 export default function NavBar() {
   return (
     <nav>
-       <img src="/palmSpaLogo.png" alt="palm spa logo" className={styles.palmLogo}/>
-        <div className={styles.hamburgerbutton }>
+      <div className={styles.palmLogo}>
+      <img src="/palmSpaLogo.png" alt="palm spa logo"/>
+
+      </div>
+        {/* <div className={styles.hamburgerbutton }>
           <Link to="#">
             <span className={styles.bar}></span>
             <span className={styles.bar}></span>
             <span className={styles.bar}></span>
           </Link>
-        </div>
+        </div> */}
 
           <div className={styles.links}>
               <Link to="/">Home</Link>
               <Link to="/spa_menu">Menu</Link>
               <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact">Book</Link>
           </div>
     </nav>
   )
